f8abef0c8bae96b7c9cc62b2073a67a05205f907
Nidhal Nouma <nidhal.nouma.0@gmail.com>
2023-07-29 21:28:49 -0400

    adding github action

--- etradingview/settings.py
+++ etradingview/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "isalgo.com",
+    "www.isalgo.com",
+    env("IP_URL"),
+]
 
 
 # Application definition
