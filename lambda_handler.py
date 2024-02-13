62d28c3fea82493256c6b671219ddd7fb6b87131
Nidhal Nouma <nidhal.nouma.0@gmail.com>
2024-02-13 18:07:51 -0500

    prepare to deploy to aws lamda

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,12 +1,4 @@
 from mangum import Mangum
 from etradingview.asgi import application
 
-# handler = Mangum(application)
-def handler(event, context):
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/plain"
-        },
-        "body": "Hello from Lambda"
-    }
+handler = Mangum(application)
