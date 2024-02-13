fb95a1a0583dfdc7e2d800bafdb120a31d2b0b2d
Nidhal Nouma <nidhal.nouma.0@gmail.com>
2024-02-13 18:27:28 -0500

    prepare to deploy to aws lamda

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,4 +1,13 @@
 from mangum import Mangum
+import json
+import logging
 from etradingview.asgi import application
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 handler = Mangum(application)
+
+def lambda_handler(event, context):
+    logger.info("Received event: " + json.dumps(event))
+    return handler(event, context)
