809451b9218a85ab153443913942731838b601a4
Nidhal Nouma <nidhal.nouma.0@gmail.com>
2024-02-15 23:34:28 -0500

    changing mangum handler

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,13 +1,5 @@
 from mangum import Mangum
-import json
-import logging
 from etradingview.asgi import application
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+handler = Mangum(application, lifespan="off")
 
-handler = Mangum(application, lifespan="off", binary_media_types=["*/*"])
-
-def lambda_handler(event, context):
-    logger.info("Received event: " + json.dumps(event))
-    return handler(event, context)
