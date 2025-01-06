0aad959ddb50e3b53589e0e93baf4ddd427be0ee
Nidhal Nouma <nidhal.nouma.0@gmail.com>
2025-01-06 12:17:59 -0500

    working in the docs styling and structure

--- profile_user/templatetags/custom_tags.py
+++ profile_user/templatetags/custom_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.loader import render_to_string
+import json
 
 register = template.Library()
 
@@ -13,3 +14,11 @@ def render_template(context, template_name, **kwargs):
     context_dict = context.flatten()
     full_context = {**context_dict, **kwargs}  # Merge the flattened context with kwargs
     return render_to_string(template_name, full_context)
+
+
+@register.filter
+def load_json(data):
+    try:
+        return json.loads(data.replace("'", "\""))
+    except ValueError:
+        return {}
